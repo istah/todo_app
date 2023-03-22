fb76c1a9b4bbf82634859df0b28e655bce247596
Issatay Massalin <massalin.issatay@gmail.com>
2023-03-22 13:22:53 +0200

    txt and file
    now lists is saved to txt file in folder

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-todos = []
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").strip()
 
     match user_action:
         case 'add':
-            todo = input('Enter a todo: ')
+            todo = input('Enter a todo: ') + '\n'
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+
             todos.append(todo)
+            
             file = open('todos.txt', 'w')
             file.writelines(todos)
 
