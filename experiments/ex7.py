1da091e7c21c149e5bb4c213aeee401542a9ffc2
Issatay Massalin <massalin.issatay@gmail.com>
2023-03-27 10:37:23 +0300

    ex and bonus files are added

---
+++ experiments/ex7.py
@@ -0,0 +1,15 @@
+# names = ["john smith", "jay santi", "eva kuki"]
+# names = [name.title() for name in names]
+# print(names)
+
+# usernames = ["john 1990", "alberta1970", "magnola2000"]
+# len = [len(l) for l in usernames]
+# print(len)
+
+# user_entries = ['10', '19.1', '20']
+# floats = [float(user_entry) for user_entry in user_entries]
+# print(floats)
+
+user_entries = ['10', '19.1', '20']
+floats = [float(x) for x in user_entries]
+print(sum(floats))
